c11bc35a6e1ad574cfd205ccc01782c2bc1aca85
Crystal Stellwagen <liraeldianne@gmail.com>
2015-06-17 13:02:10 -0700

    Working break_me file

--- hw/hw4/break_me.py
+++ hw/hw4/break_me.py
@@ -1,5 +1,11 @@
 def exhibit_name_error():
     print(pooh)
 
-def exhibit_value_error():
+
+def exhibit_type_error():
     print(3 + "x")
+
+
+def exhibit_attribute_error():
+    name = "Crystal"
+    print(name.length)
