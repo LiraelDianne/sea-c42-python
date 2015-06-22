ff3e13dfac38d4a886d976d51099a8f281317e93
Crystal Stellwagen <liraeldianne@gmail.com>
2015-06-22 16:43:34 -0700

    added sum_series function and docstring for lucas function

--- hw6/series.py
+++ hw6/series.py
@@ -13,3 +13,35 @@ def fibonacci(n):
             previous = current
             current = next
     return current
+
+
+def lucas(n):
+    """Return the nth number of the lucas series"""
+    previous = 2
+    current = 1
+    next = 0
+    if n == 1:
+        return 2
+    elif n == 2:
+        return 1
+    if n > 2:
+        for number in range(n-2):
+            next = previous + current
+            previous = current
+            current = next
+    return current
+
+
+def sum_series(n, previous=0, current=1):
+    """Return the nth number of a series where each integer is the sum of the previous two"""
+    next = 0
+    if n == 1:
+        return previous
+    elif n == 2:
+        return current
+    if n > 2:
+        for number in range(n-2):
+            next = previous + current
+            previous = current
+            current = next
+    return current
