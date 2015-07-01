2c7bd114439fc801358aa5935ca50c715ffe6a8e
Crystal Stellwagen <liraeldianne@gmail.com>
2015-07-01 13:45:13 -0700

    deals with multiple lines and selects the next word randomly from value options

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -1,3 +1,5 @@
+import random
+
 try:
     f = open('sherlock-small.txt', 'r')
     lines = f.readlines()
@@ -5,12 +7,18 @@ try:
 except FileNotFoundError as e:
     print(str(e))
 
-words = lines[0].split()
-
 d = {}
-for (i, word) in enumerate(words):
-    if i < (len(words)-2):
-        d[(words[i], words[i+1])] = words[i+2]
+
+for i in range(len(lines)):
+    words = lines[i-1].split()
+
+    for (i, word) in enumerate(words):
+        if i < (len(words)-2):
+            tup = (words[i], words[i+1])
+            if tup in d:
+                d[tup].append(words[i+2])
+            else:
+                d[tup] = [words[i+2]]
 
 start = [words[0], words[1]]
 si = 0
@@ -19,18 +27,13 @@ while going:
     if si == (len(words)-3):
         going = False
     tup = (start[si], start[si+1])
-    next_word = d[tup]
+    print(tup)
+    print(d[tup])
+    ri = random.randrange(0, len(d[tup]))
+    print(ri)
+    next_word = d[tup][ri]
     si = si + 1
     start.append(next_word)
 
-print(start)
-
-'''
-import random
-
-d = {('pick', 'three'):['words', 'clocks']}
-print(d[('pick', 'three')])
-ri = random.randint(0, len(d['pick', 'three']))
-next_word = d[('pick', 'three')][ri]
-print(next_word)
-'''
+sentence = " ".join(start)
+print(sentence)
