78ead86c6060b0830b08b62578151e3a144d8abc
Crystal Stellwagen <liraeldianne@gmail.com>
2015-07-07 16:06:03 -0700

    Not working code, but reorganized into functions and changed variable names

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -2,16 +2,23 @@ import random
 
 try:
     f = open('sherlock-small.txt', 'r')
+    for i in range(61):
+        f.readline()
     lines = f.readlines()
     f.close()
 except FileNotFoundError as e:
     print(str(e))
 
-d = {}
 
-for i in range(len(lines)):
-    words = lines[i-1].split()
+def simplify(lines):
+    wordlist = []
+    for i in range(len(lines)):
+        wordlist.append(lines[i-1].split())
+    return wordlist
 
+
+def make_trigram_dict(words):
+    d = {}
     for (i, word) in enumerate(words):
         if i < (len(words)-2):
             tup = (words[i], words[i+1])
@@ -19,21 +26,21 @@ for i in range(len(lines)):
                 d[tup].append(words[i+2])
             else:
                 d[tup] = [words[i+2]]
+    return d
 
-start = [words[0], words[1]]
-si = 0
-going = True
-while going:
-    if si == (len(words)-3):
-        going = False
-    tup = (start[si], start[si+1])
-    print(tup)
-    print(d[tup])
-    ri = random.randrange(0, len(d[tup]))
-    print(ri)
-    next_word = d[tup][ri]
-    si = si + 1
-    start.append(next_word)
 
-sentence = " ".join(start)
-print(sentence)
+def make_story(trigrams, story_length):
+    start = list(random.choice(list(trigrams.keys())))
+    si = 0
+    going = True
+    while going:
+        if si == story_length:
+            going = False
+        tup = (start[si], start[si+1])
+        select = random.randrange(0, len(trigrams[tup]))
+        next_word = trigrams[tup][select]
+        si = si + 1
+        start.append(next_word)
+
+    story = " ".join(start)
+    return story
