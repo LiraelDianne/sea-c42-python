1edbfffdf95b71ec456fa7103a7d49b7b56a201e
Crystal Stellwagen <liraeldianne@gmail.com>
2015-07-08 13:07:03 -0700

    works on full text; added docstrings

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -2,6 +2,7 @@ import random
 
 
 def simplify(lines):
+    # turn a list of lines into a list of words
     wordlist = []
     for i in range(len(lines)):
         wordlist.extend(lines[i].split())
@@ -9,6 +10,7 @@ def simplify(lines):
 
 
 def make_trigram_dict(words):
+    # turn a list of words into a trigram dictionary
     d = {}
     for (i, word) in enumerate(words):
         if i < (len(words)-2):
@@ -21,21 +23,28 @@ def make_trigram_dict(words):
 
 
 def make_story(trigrams, word_length):
+    """ Pick a random pair of words from the trigram dictionary
+    and return a story of length word_length """
     start = list(random.choice(list(trigrams.keys())))
     i = 0
     going = True
     while going:
+        # read the last two words in the start list
         tup = (start[i], start[i+1])
         if tup in trigrams:
+            # find a word that can follow the last two words
             select = random.randrange(0, len(trigrams[tup]))
             next_word = trigrams[tup][select]
             i = i + 1
             start.append(next_word)
         else:
+            # stop loop if no trigram is found
             i = word_length
         if i == word_length:
+            # stop loop when story is long enough
             going = False
 
+    # join the word list with spaces and add a period if needed
     story = " ".join(start)
     if story[len(story)-1] == ".":
         return story
@@ -45,13 +54,13 @@ def make_story(trigrams, word_length):
 
 if __name__ == "__main__":
     try:
-        f = open('sherlock-small.txt', 'r')
-        """for i in range(61):
-            f.readline() """
+        f = open('sherlock.txt', 'r')
+        for i in range(61):
+            f.readline()
         lines = f.readlines()
         f.close()
     except FileNotFoundError as e:
         print(str(e))
 
     trigram_dict = make_trigram_dict((simplify(lines)))
-    print(make_story(trigram_dict, 100))
+    print(make_story(trigram_dict, 400))
