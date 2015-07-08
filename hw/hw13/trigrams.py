9bdc77692ec3500db5045eb35433681bcbb1dd27
Crystal Stellwagen <liraeldianne@gmail.com>
2015-07-08 12:47:33 -0700

    Works on sample text

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -1,19 +1,10 @@
 import random
 
-try:
-    f = open('sherlock-small.txt', 'r')
-    for i in range(61):
-        f.readline()
-    lines = f.readlines()
-    f.close()
-except FileNotFoundError as e:
-    print(str(e))
-
 
 def simplify(lines):
     wordlist = []
     for i in range(len(lines)):
-        wordlist.append(lines[i-1].split())
+        wordlist.extend(lines[i].split())
     return wordlist
 
 
@@ -29,18 +20,38 @@ def make_trigram_dict(words):
     return d
 
 
-def make_story(trigrams, story_length):
+def make_story(trigrams, word_length):
     start = list(random.choice(list(trigrams.keys())))
-    si = 0
+    i = 0
     going = True
     while going:
-        if si == story_length:
+        tup = (start[i], start[i+1])
+        if tup in trigrams:
+            select = random.randrange(0, len(trigrams[tup]))
+            next_word = trigrams[tup][select]
+            i = i + 1
+            start.append(next_word)
+        else:
+            i = word_length
+        if i == word_length:
             going = False
-        tup = (start[si], start[si+1])
-        select = random.randrange(0, len(trigrams[tup]))
-        next_word = trigrams[tup][select]
-        si = si + 1
-        start.append(next_word)
 
     story = " ".join(start)
+    if story[len(story)-1] == ".":
+        return story
+    story += "."
     return story
+
+
+if __name__ == "__main__":
+    try:
+        f = open('sherlock-small.txt', 'r')
+        """for i in range(61):
+            f.readline() """
+        lines = f.readlines()
+        f.close()
+    except FileNotFoundError as e:
+        print(str(e))
+
+    trigram_dict = make_trigram_dict((simplify(lines)))
+    print(make_story(trigram_dict, 100))
